298d0ec0c1f9c24859aacfd882c514ffe0e44b42
DUFFAUTM <Max@10.131.128.43>
2016-06-14 10:11:10 +0200

    Commit test

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -37,7 +37,7 @@ public class View implements IView, Runnable {
 	 *          the key code
 	 * @return the controller order
 	 */
-	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
+	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) { // Controller of the key events
 		switch (keyCode) {
 			case KeyEvent.VK_G:
 				return ControllerOrder.English;
