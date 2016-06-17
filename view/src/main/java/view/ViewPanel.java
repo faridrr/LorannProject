08b3f101392eeeb3846e5b0281ab0377274e6878
DylanCa <dylan.cattelan@gmail.com>
2016-06-17 10:05:47 +0200

    View : Map Loop instantiated

--- view/src/main/java/view/ViewPanel.java
+++ view/src/main/java/view/ViewPanel.java
@@ -21,15 +21,15 @@ import javax.swing.JPanel;
 class ViewPanel extends JPanel implements Observer {
 
 	/** The view frame. */
-	private ViewFrame					viewFrame;
+	private ViewFrame viewFrame;
 	/** The Constant serialVersionUID. */
-	private static final long	serialVersionUID	= -998294702363713521L;
+	private static final long serialVersionUID = -998294702363713521L;
 
 	/**
 	 * Instantiates a new view panel.
 	 *
 	 * @param viewFrame
-	 *          the view frame
+	 *            the view frame
 	 */
 	public ViewPanel(final ViewFrame viewFrame) {
 		this.setViewFrame(viewFrame);
@@ -49,7 +49,7 @@ class ViewPanel extends JPanel implements Observer {
 	 * Sets the view frame.
 	 *
 	 * @param viewFrame
-	 *          the new view frame
+	 *            the new view frame
 	 */
 	private void setViewFrame(final ViewFrame viewFrame) {
 		this.viewFrame = viewFrame;
@@ -70,85 +70,79 @@ class ViewPanel extends JPanel implements Observer {
 	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
 	 */
 	// Example : BufferedImage NAME;
-	
-		int x = 0;
+
+	int x = 0;
 	int y = 0;
 	int pixelNumbers = 32;
-	
+
 	List<BufferedImage> Arimages = new ArrayList<BufferedImage>();
+
 	@Override
 	protected void paintComponent(final Graphics graphics) {
-		try {
-			
-			BufferedImage black = ImageIO.read(new File("src/main/resources/sprite/black.png"));
-			BufferedImage bone = ImageIO.read(new File("src/main/resources/sprite/bone.png"));
-			BufferedImage vertical_bone = ImageIO.read(new File("src/main/resources/sprite/vertical_bone.png"));
-			BufferedImage horizontal_bone = ImageIO.read(new File("src/main/resources/sprite/horizontal_bone.png"));
-			
-			BufferedImage fireball_1 = ImageIO.read(new File("src/main/resources/sprite/fireball_1.png"));
-			BufferedImage fireball_2 = ImageIO.read(new File("src/main/resources/sprite/fireball_2.png"));
-			BufferedImage fireball_3 = ImageIO.read(new File("src/main/resources/sprite/fireball_3.png"));
-			BufferedImage fireball_4 = ImageIO.read(new File("src/main/resources/sprite/fireball_4.png"));
-			
-			
-			BufferedImage gate_closed = ImageIO.read(new File("src/main/resources/sprite/gate_closed.png"));
-			BufferedImage gate_open = ImageIO.read(new File("src/main/resources/sprite/gate_open.png"));
-			BufferedImage crystal_ball = ImageIO.read(new File("src/main/resources/sprite/crystal_ball.png"));
-			BufferedImage lorann = ImageIO.read(new File("src/main/resources/sprite/lorann_b.png"));
-			
-			BufferedImage purse = ImageIO.read(new File("src/main/resources/sprite/purse.png"));
-			
-			BufferedImage monster_1 = ImageIO.read(new File("src/main/resources/sprite/monster_1.png"));
-			BufferedImage monster_2 = ImageIO.read(new File("src/main/resources/sprite/monster_2.png"));
-			BufferedImage monster_3 = ImageIO.read(new File("src/main/resources/sprite/monster_3.png"));
-			BufferedImage monster_4 = ImageIO.read(new File("src/main/resources/sprite/monster_4.png"));
-			
-			/*
-			 * Testing every images on the display
-			 */
-			Arimages.add(bone);
-			Arimages.add(black);
-			Arimages.add(vertical_bone);
-			Arimages.add(horizontal_bone);
-			Arimages.add(lorann);
-			
-			Arimages.add(fireball_1);
-			Arimages.add(fireball_2);
-			Arimages.add(fireball_3);
-			Arimages.add(fireball_4);
-			
-			Arimages.add(gate_closed);
-			Arimages.add(gate_open);
-			Arimages.add(crystal_ball);
-			Arimages.add(purse);
-			Arimages.add(monster_1);
-			Arimages.add(monster_2);
-			Arimages.add(monster_3);
-			Arimages.add(monster_4);
-			
-			
-			
-		} catch (IOException e){
 
-	}
+		for (x = 0; x < 21; x++) {
+			for (y = 0; y < 14; y++) {
+				char symbol = '-'; // {call Selectlvl1(x,y)};
+
+				switch (symbol) {
+				case '+':
+					VBone bone = new VBone(x, y);
+					Arimages.add(bone.Image());
+					// Colision ici ?
+					break;
+				case '-':
+					Hbone bone = new HBone(x, y);
+					Arimages.add(bone.Image());
+					//
+					break;
+				case '#':
+					Corners corner = new Corners(x, y);
+					Arimages.add(corner.Image());
+					//
+					break;
+				case 'x':
+					Purse purse = new Purse(x, y);
+					Arimages.add(purse.Image());
+					//
+					break;
+				case 'o':
+					CrystalBall crystal = new CrystalBall(x, y);
+					Arimages.add(crystal.Image());
+					//
+					break;
+				case '$':
+					DoorC door = new DoorC(x, y);
+					Arimages.add(door.Image());
+					//
+					break;
+				case '@':
+					DoorO door = new DoorO(x, y);
+					Arimages.add(door.Image());
+					//
+					break;
+				default:
+					Black black = new Black();
+					Arimages.add(black);
+					break;
+				}
+			}
+		}
+
 		graphics.clearRect(0, 0, this.getWidth(), this.getHeight());
-		for(Image img : Arimages){
-			
-			
-			graphics.drawImage(img,  x*pixelNumbers, y*pixelNumbers, this);
-			
-			//x++;
-			
-			//testing a conditional loop to the images display:
-			 if (x == 21){
+		for (Image img : Arimages) {
+
+			graphics.drawImage(img, x * pixelNumbers, y * pixelNumbers, this);
+
+			// x++;
+
+			// testing a conditional loop to the images display:
+			if (x == 21) {
 				x = 0;
 				y++;
-			}
-			else {
+			} else {
 				x++;
 			}
-				
-			
+
 			;
 		}
 	}
